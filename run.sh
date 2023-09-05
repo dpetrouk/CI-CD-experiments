bcea6834f1bd35b648853306a2047523a76d7721
Dmitry Petrouk <30505853+dpetrouk@users.noreply.github.com>
2023-09-05 16:38:36 +0000

    fix: try more

---
+++ run.sh
@@ -0,0 +1,8 @@
+docker run -d -it --name=kz kangzeroo
+
+
+# docker run -ti --rm -v ./shared:/shared alpine:3.10
+
+
+# docker volume create --name DataVolume1
+# docker run -ti --rm -v DataVolume1:/datavolume1 alpine:3.10
